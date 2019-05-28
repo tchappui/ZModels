83fc89fa2e5755277ccf5c74da9b271f40b839e4
Thierry Chappuis <thierry@chappuis.io>
2019-05-28 08:43:09 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/tchappui/zmodels',
+    url='https://github.com/tchappui/ZModels',
     version='0.1.0',
     zip_safe=False,
 )
